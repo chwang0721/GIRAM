f3d03adb7d81e120d475e5dad5982464fdf0b934
Chenhao Wang <chwang@std.uestc.edu.cn>
2025-05-27 16:52:08 +0800

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -2,19 +2,19 @@
 
 python preprocess.py --dataset NYC &
 python preprocess.py --dataset TKY &
-python preprocess.py --dataset Gowalla &
+python preprocess.py --dataset CA &
 
 wait
 
 python build_graph.py --dataset NYC &
 python build_graph.py --dataset TKY &
-python build_graph.py --dataset Gowalla &
+python build_graph.py --dataset CA &
 
 wait
 
 python ui_graph.py --dataset NYC &
 python ui_graph.py --dataset TKY &
-python ui_graph.py --dataset Gowalla &
+python ui_graph.py --dataset CA &
 
 wait
 
